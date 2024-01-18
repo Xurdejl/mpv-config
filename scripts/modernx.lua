92f433a46776983ffc22aaf12fcebb674847e2e9
Xurdejl <43864935+Xurdejl@users.noreply.github.com>
2024-01-18 01:16:43 +0100

    replace deprecated shared-script-properties

--- scripts/modernx.lua
+++ scripts/modernx.lua
@@ -2527,7 +2527,7 @@ function visibility_mode(mode, no_osd)
     end
 
     user_opts.visibility = mode
-    utils.shared_script_property_set("osc-visibility", mode)
+    mp.set_property_native("user-data/osc/visibility", mode)
 
     if not no_osd and tonumber(mp.get_property("osd-level")) >= 1 then
         mp.osd_message("OSC visibility: " .. mode)
