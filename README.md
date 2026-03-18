f7a8875596cccd1f5774f0f05f900a446f73dd28
xurdejl <xurde86@gmail.com>
2026-03-18 19:05:59 +0100

    update silentskip to include chapter aware skip

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center">~ 🌑 𝓜𝓟𝓥 𝓒𝓸𝓭𝓮𝔁 🌑 ~</h1> 
+<h1 align="center">~ 🌑 𝓜𝓟𝓥 𝓒𝓸𝓭𝓮𝔁 🌑 ~</h1>
 
 <h3 align="center"><samp>&gt;  Just a set of scripts and configs, because default settings are boring.</samp></h3>
 
@@ -9,20 +9,19 @@
 ## 🌨️ Shaders
 
 - **[FSRCNN](https://github.com/igv/FSRCNN-TensorFlow/releases)** - Prescaler based on layered convolutional networks.
-    
--   **[SSimDownscaler, SSimSuperRes, Krig, Adaptive Sharpen, etc.](https://gist.github.com/igv)**
-    
-    -   **[SSimDownscaler](https://gist.github.com/igv/36508af3ffc84410fe39761d6969be10)** - Perceptually based downscaler.
-    -   **[SSimSuperRes](https://gist.github.com/igv/2364ffa6e81540f29cb7ab4c9bc05b6b)** - The aim of this shader is to make corrections to the image upscaled by mpv built-in scaler (removes ringing artifacts, restores original sharpness, etc).
-    -   **[Krig](https://gist.github.com/igv/a015fc885d5c22e6891820ad89555637)** - Chroma scaler that uses luma information for high quality upscaling.
+
+- **[SSimDownscaler, SSimSuperRes, Krig, Adaptive Sharpen, etc.](https://gist.github.com/igv)**
+  - **[SSimDownscaler](https://gist.github.com/igv/36508af3ffc84410fe39761d6969be10)** - Perceptually based downscaler.
+  - **[SSimSuperRes](https://gist.github.com/igv/2364ffa6e81540f29cb7ab4c9bc05b6b)** - The aim of this shader is to make corrections to the image upscaled by mpv built-in scaler (removes ringing artifacts, restores original sharpness, etc).
+  - **[Krig](https://gist.github.com/igv/a015fc885d5c22e6891820ad89555637)** - Chroma scaler that uses luma information for high quality upscaling.
 
 ## 🔮 Scripts
 
-- **[hayase-osc](./scripts/hayase-osc.lua)** - My custom osc fork, designed to look and feel like hayase.watch’s ui *(forked from [ModernZ](https://github.com/Samillion/ModernZ))*.
+- **[hayase-osc](./scripts/hayase-osc.lua)** - Custom OSC skin inspired by [hayase.watch](https://hayase.watch)'s UI _(forked from [ModernZ](https://github.com/Samillion/ModernZ))_.
 
 - **[evafast](https://github.com/po5/evafast)** - Fast-forwarding and seeking on a single key, with quality of life features like a slight slowdown when subtitles are shown.
 
-- **[silentskip](./scripts/silentskip.lua)** - Effortlessly skip episode openings with a simple TAB press. *(Forked from [skiptosilence.lua](https://github.com/dyphire/mpv-config/blob/master/scripts/skiptosilence.lua))*
+- **[silentskip](./scripts/silentskip.lua)** - Skip intro/ending with silence detection fallback via hotkey.
 
 - **[thumbfast](https://github.com/po5/thumbfast)** - High-performance on-the-fly thumbnailer script for mpv.
 
--- scripts/silentskip.lua
+++ scripts/silentskip.lua
@@ -1,250 +1,230 @@
--- silentskip.lua
---
--- Forked from skiptosilence.lua (github.com/dyphire/mpv-config)
--- Original authors: detuur, microraptor, Eisa01, dyphire
--- License: MIT
---
--- Fork by: xurdejl
+--[[
+    silentskip.lua
+    Skip to next silence with chapter aware skip.
+    License: MIT
 
-local mp = require 'mp'
-local msg = require 'mp.msg'
-local utils = require 'mp.utils'
+    Based on skiptosilence.lua (github.com/detuur/mpv-scripts)
+    Chapter matching adapted from chapterskip.lua (github.com/po5/chapterskip)
+    Fork by: xurdejl
+--]]
 
---Configuration
-local options = {
-    -- Audio detection settings
-    silence_audio_level = -40,         -- Audio level in dB that is considered silence
-    silence_duration = 0.65,           -- Duration in seconds needed to detect silence
-    ignore_silence_duration = 5,       -- Ignore silences in first X seconds from start position
-    
-    -- Skip duration limits
-    min_skip_duration = 0,             -- Minimum skip duration (0 = no minimum)
-    max_skip_duration = 120,           -- Maximum skip duration (0 = no maximum)
-    
-    -- User experience
-    keybind_twice_cancel_skip = true,  -- Cancel skip by pressing keybind twice
-    force_mute_on_skip = false,        -- Mute audio during skip
-    osd_duration = 2500,               -- OSD message duration in ms
-    osd_msg = false,                   -- Show OSD messages
-    skip_feedback_on_success = true,   -- Show feedback message on successful skip
-    silence_skip_keybind = "Tab",      -- Keybind to trigger skip to silence
+local mp = require "mp"
+local msg = require "mp.msg"
+
+local categories = {
+    prologue = "^[Pp]rologue/^[Ii]ntro",
+    opening = "^OP/ OP$/^[Oo]pening/[Oo]pening$/^Intro%s*Start/オープニング$/^片头$/片头开始$",
+    ending = "^ED/ ED$/^[Ee]nding/[Ee]nding$/エンディング$",
+    credits = "^[Cc]redits/[Cc]redits$",
+    preview = "[Pp]review$",
 }
 
--- State Variables
+local options = {
+    skip_keybind = "Tab",      -- Keybind to trigger skip
+    silence_audio_level = -40, -- Audio level (dB) considered as silence
+    silence_duration = 0.65,   -- Duration (seconds) of silence needed to trigger skip
+    min_skip_lead = 5,         -- Seconds after skip start before silence is accepted
+    max_skip_duration = 90,    -- Seconds before giving up and aborting silence scan
+}
+
 local state = {
     skip_active = false,
     initial_skip_time = 0,
-    original = {
-        speed = 1,
-        pause = false,
-        mute = false,
-        sub = nil,
-        secondary_sub = nil,
-        vid = nil,
-        window = nil
-    },
-    timer = nil
+    original = {},
+    timer = nil,
 }
 
---Helper Functions
-local function show_message(text, duration)
-    if not text then return end
-    duration = duration or options.osd_duration
-    
-    if options.osd_msg then 
-        mp.commandv("show-text", text, duration) 
+local handle_silence_detection
+
+local function window_is_large()
+    return mp.get_property_native("fullscreen")
+        or mp.get_property_native("window-maximized")
+end
+
+local function lock_window_size()
+    if window_is_large() then return end
+    local w = mp.get_property_native("osd-width")
+    local h = mp.get_property_native("osd-height")
+    if w and h then
+        mp.set_property_native("geometry", string.format("%dx%d", w, h))
     end
-    msg.info(text)
+end
+
+local function unlock_window_size()
+    mp.set_property("geometry", "")
 end
 
 local function save_state()
-    state.original.sub = mp.get_property("sub-visibility")
-    state.original.secondary_sub = mp.get_property("secondary-sub-visibility")
-    state.original.window = mp.get_property("force-window")
+    state.original.sub = mp.get_property_bool("sub-visibility")
+    state.original.secondary_sub = mp.get_property_bool("secondary-sub-visibility")
+    state.original.force_window = mp.get_property("force-window")
     state.original.vid = mp.get_property("vid")
     state.original.mute = mp.get_property_native("mute")
     state.original.pause = mp.get_property_native("pause")
     state.original.speed = mp.get_property_native("speed")
 end
 
-local function apply_skip_settings()
-    mp.set_property("sub-visibility", "no")
-    mp.set_property("secondary-sub-visibility", "no")
-    mp.set_property("force-window", "yes")
-    mp.set_property("vid", "no")
-    
-    if options.force_mute_on_skip then
-        mp.set_property_bool("mute", true)
-    end
-    
-    mp.set_property_bool("pause", false)
-    mp.set_property("speed", 100)
-    
-    -- Setup the silencedetect filter
-    mp.command(
-        "no-osd af add @skiptosilence:lavfi=[silencedetect=noise=" ..
-        options.silence_audio_level .. "dB:d=" .. options.silence_duration .. "]"
-    )
-    
-    mp.observe_property("af-metadata/skiptosilence", "string", handle_silence_detection)
-end
+local function restore_state(timepos)
+    timepos = timepos or state.initial_skip_time
 
-local function restore_state(timepos, use_saved_pause_state)
-    if not timepos then 
-        timepos = mp.get_property_number("time-pos") or state.initial_skip_time
-    end
-    
-    local pause_state = use_saved_pause_state and state.original.pause or false
-    
-    -- Restore all properties
     mp.set_property("vid", state.original.vid)
-    mp.set_property("force-window", state.original.window)
+    mp.set_property("force-window", state.original.force_window)
     mp.set_property_bool("mute", state.original.mute)
     mp.set_property("speed", state.original.speed)
-    mp.set_property("sub-visibility", state.original.sub)
-    mp.set_property("secondary-sub-visibility", state.original.secondary_sub)
-    
-    -- Clean up filter and observers
+    mp.set_property_bool("sub-visibility", state.original.sub)
+    mp.set_property_bool("secondary-sub-visibility", state.original.secondary_sub)
+    mp.set_property_bool("pause", state.original.pause)
+
     mp.unobserve_property(handle_silence_detection)
     mp.command("no-osd af remove @skiptosilence")
-    
-    mp.set_property_bool("pause", pause_state)
-    
-    -- Ensure time-pos is valid before setting
+
     local duration = mp.get_property_number("duration") or 0
     if timepos >= 0 and timepos <= duration then
         mp.set_property_number("time-pos", timepos)
     end
-    
-    if state.timer then 
-        state.timer:kill() 
+
+    if state.timer then
+        state.timer:kill()
         state.timer = nil
     end
-    
+
     state.skip_active = false
 end
 
-local function check_duration_limits(current_time)
-    if not state.skip_active then return false end
-    
-    current_time = current_time or mp.get_property_number("time-pos") or 0
-    local skip_duration = current_time - state.initial_skip_time
-    
-    -- Check minimum skip duration
-    if options.min_skip_duration > 0 and skip_duration <= options.min_skip_duration then
-        restore_state(state.initial_skip_time)
-        show_message('Skip cancelled: Silence less than minimum duration')
-        return true
+local function apply_skip_settings()
+    mp.set_property_bool("sub-visibility", false)
+    mp.set_property_bool("secondary-sub-visibility", false)
+    mp.set_property("force-window", "yes")
+    mp.set_property("vid", "no")
+    mp.set_property_bool("pause", false)
+    mp.set_property("speed", 100)
+
+    mp.command(
+        "no-osd af add @skiptosilence:lavfi=[silencedetect=noise="
+        .. options.silence_audio_level
+        .. "dB:d=" .. options.silence_duration .. "]"
+    )
+
+    mp.observe_property("af-metadata/skiptosilence", "string", handle_silence_detection)
+end
+
+local function chapter_title_matches(title)
+    if not title then return false end
+    for _, patterns in pairs(categories) do
+        for pattern in patterns:gmatch("([^/]+)") do
+            if title:find(pattern) then return true end
+        end
     end
-    
-    -- Check maximum skip duration
-    if options.max_skip_duration > 0 and skip_duration >= options.max_skip_duration then
-        restore_state(state.initial_skip_time)
-        show_message('Skip cancelled: Maximum skip duration reached')
-        return true
-    end
-    
     return false
 end
 
--- Main Functions
-function trigger_silence_skip()
-    -- If already skipping and double press cancels, restore and exit
-    if state.skip_active and options.keybind_twice_cancel_skip then 
-        restore_state(state.initial_skip_time)
-        show_message('Skip cancelled by user')
-        return 
+local function find_chapter_skip_target(current_time)
+    local chapter_list = mp.get_property_native("chapter-list")
+    if not chapter_list or #chapter_list == 0 then return nil end
+
+    local n = #chapter_list
+    local duration = mp.get_property_number("duration") or 0
+
+    local function chapter_end(i)
+        return chapter_list[i + 1] and chapter_list[i + 1].time or duration
     end
-    
-    state.initial_skip_time = mp.get_property_number("time-pos") or 0
-    local duration = mp.get_property_number('duration') or 0
-    
-    -- Don't start skip if at the end of the file
-    if duration > 0 and math.floor(state.initial_skip_time) >= math.floor(duration) - 1 then
-        show_message('Cannot skip: Already at end of file')
-        return
-    end
-    
-    local is_fullscreen = mp.get_property_native("fullscreen")
-    if not is_fullscreen then
-        local width = mp.get_property_native("osd-width")
-        local height = mp.get_property_native("osd-height")
-        if width and height then
-            mp.set_property_native("geometry", ("%dx%d"):format(width, height))
+
+    for i = 1, n do
+        local cstart = chapter_list[i].time
+        local cend = chapter_end(i)
+        -- Only match the chapter the user is currently inside.
+        if current_time >= cstart and current_time < cend then
+            if chapter_title_matches(chapter_list[i].title) then
+                local last = i
+                while last < n and chapter_title_matches(chapter_list[last + 1].title) do
+                    last = last + 1
+                end
+                local target = chapter_end(last)
+                msg.info(string.format("Chapter skip: '%s' [%.2fs -> %.2fs]",
+                    chapter_list[i].title, chapter_list[i].time, target))
+                return target
+            end
+            return nil
         end
     end
-    
-    save_state()
-    
-    apply_skip_settings()
-    
-    state.skip_active = true
-    
-    state.timer = mp.add_periodic_timer(0.5, function()
-        if not state.skip_active then return end
-        
-        local video_time = mp.get_property_number("time-pos")
-        if not video_time then return end
-        
-        check_duration_limits(video_time)
-    end)
-    
-    show_message('Skipping to next silence...')
+    return nil
 end
 
-function handle_silence_detection(name, value)
+handle_silence_detection = function(_, value)
     if not state.skip_active then return end
     if not value or value == "{}" then return end
-    
-    local timecode = tonumber(string.match(value, "%d+%.?%d+"))
+
+    local timecode = tonumber(value:match("%d+%.?%d+"))
     if not timecode then return end
-    
-    if timecode < state.initial_skip_time + options.ignore_silence_duration then
-        return
-    end
-    
-    if check_duration_limits(timecode) then return end
-    
+
+    if timecode < state.initial_skip_time + options.min_skip_lead then return end
+
     restore_state(timecode)
-    
-    if options.skip_feedback_on_success then
-        mp.add_timeout(0.05, function() 
-            show_message('Skipped to silence 🕒 ' .. mp.get_property_osd("time-pos"))
-        end)
-    end
 end
 
-function handle_end_of_file(name, val)
-    if val and state.skip_active then
-        show_message('Skip cancelled: End of file reached')
+local function handle_pause_change(_, value)
+    if value and state.skip_active then
         restore_state(state.initial_skip_time)
     end
 end
 
-function handle_pause_change(name, value)
-    if value and state.skip_active then
-        show_message('Skip cancelled: Playback paused')
-        restore_state(state.initial_skip_time, true)
+local function trigger_silence_skip()
+    if state.skip_active then return end
+
+    local current_time = mp.get_property_number("time-pos") or 0
+    local duration = mp.get_property_number("duration") or 0
+
+    -- Do nothing if we're already at (or past) the last second.
+    if duration > 0 and math.floor(current_time) >= math.floor(duration) - 1 then
+        return
     end
+
+    -- Chapter match: instant seek, no silence detection required.
+    local target = find_chapter_skip_target(current_time)
+    if target then
+        mp.set_property_number("time-pos", target)
+        return
+    end
+
+    -- If less time remains than the max scan window, just jump to 1s before
+    -- the end so the file finishes naturally without triggering a scan.
+    if duration > 0 and (duration - current_time) < options.max_skip_duration then
+        mp.set_property_number("time-pos", duration - 1)
+        return
+    end
+
+    -- Start silence-detection fast-forward.
+    state.initial_skip_time = current_time
+    state.skip_active = true
+
+    lock_window_size()
+    save_state()
+    apply_skip_settings()
+
+    state.timer = mp.add_periodic_timer(0.05, function()
+        if not state.skip_active then return end
+        local pos = mp.get_property_number("time-pos")
+        if pos and (pos - state.initial_skip_time) >= options.max_skip_duration then
+            restore_state(pos)
+        end
+    end)
 end
 
-function reset_on_file_load()
-    local is_fullscreen = mp.get_property_native("fullscreen")
-    if not is_fullscreen then
-        mp.set_property("geometry", "")
-    end
-
+local function on_end_file()
     if state.skip_active then
-        show_message('Skip cancelled: New file loaded')
-        restore_state(0)
+        restore_state(nil)
     end
+    unlock_window_size()
 end
 
--- Event Registration
-mp.register_event('file-loaded', reset_on_file_load)
+local function on_file_loaded()
+    if state.skip_active then
+        restore_state(nil)
+    end
+    unlock_window_size()
+end
 
-mp.observe_property('pause', 'bool', handle_pause_change)
-
-mp.observe_property('eof-reached', 'bool', handle_end_of_file)
-
-mp.add_key_binding(options.silence_skip_keybind, "silence-skip", trigger_silence_skip)
+mp.register_event("end-file", on_end_file)
+mp.register_event("file-loaded", on_file_loaded)
+mp.observe_property("pause", "bool", handle_pause_change)
+mp.add_key_binding(options.skip_keybind, "silence-skip", trigger_silence_skip)
