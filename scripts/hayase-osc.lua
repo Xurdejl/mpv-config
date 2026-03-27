cb2158f378d0206155fe1f23d7e5c8743956a969
xurdejl <xurde86@gmail.com>
2026-03-27 21:43:12 +0100

    fix: clear text_width_cache in osc_init

--- scripts/hayase-osc.lua
+++ scripts/hayase-osc.lua
@@ -2054,6 +2054,8 @@ end
 local function osc_init()
     msg.debug("osc_init")
 
+    text_width_cache = {}  -- invalidate cache on init (resolution may change)
+
     setup_canvas()
     create_elements()
 
